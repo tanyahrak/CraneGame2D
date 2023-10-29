a396ee526f11f0b780eceb22082406c88eca151a
tanyahrak <taniharakosuke@gmail.com>
2023-10-29 14:55:31 +0900

    マウスイベント整理 ボタンがないところでMouseUpしたときのため

--- script/buttonController.js
+++ script/buttonController.js
@@ -9,13 +9,10 @@ let moveDirection = myDirection.none;
 let armIsOpening = false;
 
 document.getElementById("left").addEventListener ("mousedown", () => {moveDirection = myDirection.left}); 
-document.getElementById("left").addEventListener ("mouseup", () => {moveDirection = myDirection.none});
 document.getElementById("right").addEventListener ("mousedown", () => {moveDirection = myDirection.right}); 
-document.getElementById("right").addEventListener ("mouseup", () => {moveDirection = myDirection.none});
 document.getElementById("up").addEventListener ("mousedown", () => {moveDirection = myDirection.up}); 
-document.getElementById("up").addEventListener ("mouseup", () => {moveDirection = myDirection.none});
 document.getElementById("down").addEventListener ("mousedown", () => {moveDirection = myDirection.down}); 
-document.getElementById("down").addEventListener ("mouseup", () => {moveDirection = myDirection.none});
+document.addEventListener ("mouseup", () => {moveDirection = myDirection.none});
 
-document.getElementById("open").addEventListener ("mouseup", () => {armIsOpening = true});
-document.getElementById("close").addEventListener ("mouseup", () => {armIsOpening = false});
+document.getElementById("open").addEventListener ("click", () => {armIsOpening = true});
+document.getElementById("close").addEventListener ("click", () => {armIsOpening = false});
